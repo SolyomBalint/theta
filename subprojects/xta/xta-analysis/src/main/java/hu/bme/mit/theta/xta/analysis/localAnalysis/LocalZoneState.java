9fd0b060dc01e138d46166e19a6793e974a38829
balint.solyom <balint.solyom01@gmail.com>
2024-07-26 14:45:10 +0200

    Added subdir for local analysis and updated gitignore so that bins are not commited

---
+++ subprojects/xta/xta-analysis/src/main/java/hu/bme/mit/theta/xta/analysis/localAnalysis/LocalZoneState.java
@@ -0,0 +1,21 @@
+package hu.bme.mit.theta.xta.analysis.localAnalysis;
+
+import hu.bme.mit.theta.analysis.expr.ExprState;
+import hu.bme.mit.theta.core.type.Expr;
+import hu.bme.mit.theta.core.type.booltype.BoolType;
+
+public final class LocalZoneState implements ExprState {
+
+    @Override
+    public Expr<BoolType> toExpr() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isBottom() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
